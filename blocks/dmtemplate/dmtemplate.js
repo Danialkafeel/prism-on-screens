ad5b7c23c3f5dde9e1f4dadb50428773069389b0
Abhinav Saraswat <abhinavsaraswat@Abhinavs-MacBook-Pro-6.local>
2023-12-06 00:12:30 +0530

    Updating lint issues

--- blocks/dmtemplate/dmtemplate.js
+++ blocks/dmtemplate/dmtemplate.js
@@ -1,14 +1,11 @@
 import { createOptimizedPicture } from '../../scripts/lib-franklin.js';
 
 export default async function decorate(block) {
-
-    //block.setAttribute('style', 'display:none');
-    const column = block.firstElementChild;
-    const templateDiv = column.firstElementChild;
-    column.setAttribute('style', 'display:none');
-    const templateName = templateDiv.innerText;
-    const imageName = "/dmtemplate/" + templateName;
-    const img = createOptimizedPicture(imageName);
-    block.appendChild(img);
-    console.log("Template Name = " + templateName);
+  const column = block.firstElementChild;
+  const templateDiv = column.firstElementChild;
+  column.setAttribute('style', 'display:none');
+  const templateName = templateDiv.innerText;
+  const imageName = `/dmtemplate/${templateName}`;
+  const img = createOptimizedPicture(imageName);
+  block.appendChild(img);
 }
