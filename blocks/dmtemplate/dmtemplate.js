42f4fcc8aca22d1f1e3c0faf87339e9e5b74fb3b
Dhaval Kumar <dhavkuma@adobe.com>
2023-12-11 20:16:25 +0530

    fix: rowId not init properlu

--- blocks/dmtemplate/dmtemplate.js
+++ blocks/dmtemplate/dmtemplate.js
@@ -1,19 +1,25 @@
 import { createOptimizedPicture } from '../../scripts/lib-franklin.js';
 export default async function decorate(block) {
   block.setAttribute('data-item-length', block.children.length);
+
+  let rowId = 0;
+  
   [...block.children].forEach(row => {
     const DEFAULT_DISPLAY_DURATION = 5000;//5 ms 
-    let rowId = 0;
+  
     row.setAttribute('style', 'display:none');
     row.setAttribute('data-item-id', ++rowId);    
+  
     const columns = [...row.children];
     columns[0].setAttribute('style', 'display:none');
     columns[1].setAttribute('style', 'display:none');    
+  
     let templateName = columns[1]?.firstElementChild?.innerText;
-    console.log(`dhaval ${templateName}`);
+  
     if (!templateName) {
       templateName = 'pmodiCo/WelcomePage';
     }
+  
     const imageName = `/is/image/${templateName}`;
     const img = createOptimizedPicture(imageName);
     
@@ -40,7 +46,7 @@ export default async function decorate(block) {
           }, duration);
       }
   }
-  
+
   function findNextVisibleItem() {
       let visibleItem = [...block.children].find((row) => row.getAttribute('style') === 'display:block');
       if (visibleItem) {
