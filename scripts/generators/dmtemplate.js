c7c87a412c72a51870c95ecdd7cb2f68c3563db5
danialkafee <danialkafee@adobe.com>
2023-12-15 18:05:41 +0530

    fix: add dmtemplate path from doc to manifest

--- scripts/generators/dmtemplate.js
+++ scripts/generators/dmtemplate.js
@@ -12,10 +12,18 @@ export default class HtmlGenerator {
     // Get default franklin markup for path
       const franklinString = await getFranklinMarkup(host, path);
       const $ = load(franklinString);
+      const dmtemplateAssets = $('div.dmtemplate');
+      for (const dmtemplateAsset of dmtemplateAssets) {
+        // check second child div of dmtemplateAsset
+        const dmtemplateAssetChild = dmtemplateAsset.children[1];
+        console.log('dmtemplateAssetChild', dmtemplateAssetChild.innerHTML);
+        if (dmtemplateAssetChild.innerHTML !== ''){
+          additionalAssets.push(`/is/image/${dmtemplateAssetChild.innerHTML}`);
+        }
+      }
       await ensureDir(p.dirname(path));
       await outputFile(`${path}.html`, $.html());
       additionalAssets.push('/blocks/dmtemplate/dmtemplate.js');
-      additionalAssets.push(`/is/image/pmodiCo/OfficeSite`);
       additionalAssets.push('/blocks/dmtemplate/dmtemplate.css');
     } catch (error) {
       console.error(error);
